7e7f8046d68dfc07e1808c71f0a97a6c247eea74
stivex001 <stephenadeyemo@gmail.com>
2023-05-09 14:56:07 +0100

    fix: typo fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,8 +21,9 @@ const Navbar = () => {
           }}
         >
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
-          <p className="text-white text-[18px] font-bold cursor-pointer">
-            Gabriel <span className="sm:block hidden">| Elufidodo</span>
+          <p className="text-white text-[18px] font-bold cursor-pointer flex">
+            Gabriel &nbsp;
+            <span className="sm:block hidden">| Elufidodo</span>
           </p>
         </Link>
         <ul className="list-none hidden sm:flex gap-10">
